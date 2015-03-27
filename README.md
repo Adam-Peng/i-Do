c0301c8d875323c55d752659d4fa23ccafcabaa3
Bo Peng <pengboadam@hotmail.com>
2015-03-27 02:05:30 -0700

    git initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# i Do (A To Do mobile app using AngularJs and PhoneGap)
+
+#Functions
+
+1. add ToDo
+2. remove ToDo
+3. show Completed
+4. remove Completed
+5. Date pick up
---
+++ module.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Vivian on 2015-03-26.
+ */
+var toDo = angular.module('toDo',[]);
+
+
+
+toDo.controller('toDoCtrl', function($scope){
+
+    $scope.todoList = [{todoText:'Clean House', done:false}];
+
+    $scope.todoAdd = function() {
+        $scope.todoList.push({todoText:$scope.todoInput, done:false});
+        $scope.todoInput = "";
+    };
+
+    $scope.remove = function() {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.todoList.push(x);
+        });
+    };
+});
+
+
